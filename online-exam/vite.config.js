26c5ff63a836b0724faee893d16e9b0150c2bfae
Prashant Shrivastava <tera-real-email@example.com>
2025-08-25 16:49:58 +0530

    last again

--- online-exam/vite.config.js
+++ online-exam/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/online-exam/',
+  base: './',   // This ensures relative paths for Netlify deployment
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
